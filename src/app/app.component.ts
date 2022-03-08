327650281a07fdfda1d8de2fcaa5a3522d6f5f44
Alejandro Cerezo <alce65@hotmail.es>
2022-03-08 10:25:39 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isdi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '01-ng-todo';
+}
